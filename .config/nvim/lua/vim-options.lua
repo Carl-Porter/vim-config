046d54cbb0c68268096e1dfe559caa760aae391f
Carl <cep79@nau.edu>
2025-01-28 18:45:13 -0500

    Fixed signcolumn overrides

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -2,11 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.cmd("set signcolumn=number")
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.g.mapleader = " "
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
