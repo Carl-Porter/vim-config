836dec826c9602e75bc97eeadaa1c7bc0dd0d825
Carl <cep79@nau.edu>
2025-01-28 19:08:47 -0500

    keymaps and base settings

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,12 +1,26 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.wrap = false
+
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.g.mapleader = " "
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
 
+vim.opt.scrolloff = 8
+
+vim.g.mapleader = " "
+
+--keep cursor in middle with half page jumps
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+ 
+--keep cursor in middle with next search term
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
