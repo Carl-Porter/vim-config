200a275bfe3494bdb9a55bf3410ce9670d06a2fb
Carl <124603049+Carl-Porter@users.noreply.github.com>
2025-04-09 17:36:19 -0400

    Update vim-options.lua
    Add autowrap for .md files

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,6 +3,18 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
+--For obsidian.nvim to format markdown
+vim.opt.conceallevel = 1
+
+-- autocmds to autowrap .md files only
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = { '*.md' },
+  callback = function()
+    vim.opt.colorcolumn = '80'
+    vim.opt.textwidth = 80
+  end,
+})
+
 vim.opt.wrap = false
 
 vim.opt.number = true
